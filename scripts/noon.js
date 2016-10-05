a6116f510cad1f8a8834b587238a69d69f0d9185
Leo <leo.lee7654@gmail.com>
2016-10-05 17:00:58 +0900

    Update noon.js

--- scripts/noon.js
+++ scripts/noon.js
@@ -5,12 +5,25 @@
 // @version      0.1
 // @description  try to take over the world!
 // @author       You
-// @match        *://noondate.com/v2/cards/today
+// @match        *://noondate.com/*
 // @grant        none
+// @require      http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js
 // ==/UserScript==
 
 (function() {
     'use strict';
-alert();
+    var pathArray = window.location.pathname.split( '/' );
+    function getIdx() {
+        return pathArray[3];
+    }
+    var idx = getIdx();
+    $.post('http://noondate.com/v2/dialogs/user_impression', {idx:idx, gender:1}, function(res){
+        if (res.indexOf('이미 평가한') > 0) {
+            console.debug('이미평가');
+        } else {
+            console.debug('아직평가');
+        }
+    });
+
     // Your code here...
 })();
