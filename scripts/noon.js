d7c9cdc29682f3c7687465bc84c07cb232739518
Leo <leo.lee7654@gmail.com>
2016-10-05 16:40:21 +0900

    Update noon.js

--- scripts/noon.js
+++ scripts/noon.js
@@ -1,1 +1,16 @@
+// ==UserScript==
+// @name         Noon
+// @downloadURL  https://raw.githubusercontent.com/Leo7654/TMScripts/master/scripts/noon.js
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        *://noondate.com/v2/cards/today
+// @grant        none
+// ==/UserScript==
 
+(function() {
+    'use strict';
+alert();
+    // Your code here...
+})();
